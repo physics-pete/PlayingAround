701933d939343cd08b19a521fdcfd5ea67a11a5d
Peter Gruszka <info@peter-gruszka.de>
2021-02-22 22:59:42 +0100

    Initial Commit

---
+++ Shared/PlayingAroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayingAroundApp.swift
+//  Shared
+//
+//  Created by Peter Gruszka on 22.02.21.
+//
+
+import SwiftUI
+
+@main
+struct PlayingAroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
